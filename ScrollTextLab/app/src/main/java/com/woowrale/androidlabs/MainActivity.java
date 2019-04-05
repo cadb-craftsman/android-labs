211b4dbc7dd1d68fbe843cdedc854de419c438a4
woowrale <woowrale@gmail.com>
2019-04-05 10:38:09 +0200

    Add ScrollTextLab to repository

---
+++ ScrollTextLab/app/src/main/java/com/woowrale/androidlabs/MainActivity.java
@@ -0,0 +1,15 @@
+package com.woowrale.androidlabs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.woowrale.scrolltextlab.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
