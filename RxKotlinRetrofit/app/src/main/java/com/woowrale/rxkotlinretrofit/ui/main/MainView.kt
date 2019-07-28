84c4283984e874e752345cd345be5156ef13c1d3
Carlos Diaz <Carlos@localhost.localdomain>
2019-07-28 22:44:18 +0200

    Update project files

--- RxKotlinRetrofit/app/src/main/java/com/woowrale/rxkotlinretrofit/ui/main/MainView.kt
+++
@@ -1,8 +0,0 @@
-package com.woowrale.rxkotlinretrofit.ui.main
-
-import com.woowrale.rxkotlinretrofit.data.model.Market
-import com.woowrale.rxkotlinretrofit.ui.base.BasePresenter
-
-interface MainView: BasePresenter.View {
-    fun setRecyclerViewAdapterData(marketList: List<Market>)
-}
