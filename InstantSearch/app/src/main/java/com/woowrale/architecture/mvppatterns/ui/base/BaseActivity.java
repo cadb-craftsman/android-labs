5c9f8d562d50b1b6d639fe3d276cc6a6a14bc27b
Carlos Diaz <Carlos@localhost.localdomain>
2019-08-27 23:34:54 +0200

    Update project files

--- InstantSearch/app/src/main/java/com/woowrale/architecture/mvppatterns/ui/base/BaseActivity.java
+++ InstantSearch/app/src/main/java/com/woowrale/architecture/mvppatterns/ui/base/BaseActivity.java
@@ -2,12 +2,7 @@ package com.woowrale.architecture.mvppatterns.ui.base;
 
 import android.support.v7.app.AppCompatActivity;
 
-import com.woowrale.architecture.mvppatterns.MainApplication;
 import com.woowrale.architecture.mvppatterns.data.repository.ApiClient;
-import com.woowrale.architecture.mvppatterns.di.components.DaggerMainApplicationComponent;
-import com.woowrale.architecture.mvppatterns.di.components.MainApplicationComponent;
-
-import javax.inject.Inject;
 
 import retrofit2.Retrofit;
 
