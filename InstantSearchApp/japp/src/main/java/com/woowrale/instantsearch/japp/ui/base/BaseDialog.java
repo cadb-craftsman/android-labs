ebc2f2987fda187eaefd5449a9da479a0f897d41
Woowrale <woowrale@gmail.com>
2020-04-08 10:32:30 +0200

    Development only one project with two modules a kotlin module and java module for the same app.

--- InstantSearchApp/japp/src/main/java/com/woowrale/instantsearch/japp/ui/base/BaseDialog.java
+++
@@ -1,6 +0,0 @@
-package com.woowrale.instantsearch.japp.ui.base;
-
-import android.support.v4.app.DialogFragment;
-
-public abstract class BaseDialog extends DialogFragment {
-}
--- InstantSearchApp/japp/src/main/java/com/woowrale/instantsearch/japp/ui/base/BaseFragment.java
+++
@@ -1,6 +0,0 @@
-package com.woowrale.instantsearch.japp.ui.base;
-
-import android.support.v4.app.Fragment;
-
-public abstract class BaseFragment extends Fragment implements BasePresenter.View {
-}
