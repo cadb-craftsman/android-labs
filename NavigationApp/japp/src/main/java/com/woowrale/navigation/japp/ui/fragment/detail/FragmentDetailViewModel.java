bac70aa2abf0180fe23d39a1c302a9f5fa5d77a6
Carlos A. Diaz <woowrale@gmail.com>
2020-07-08 15:15:12 +0200

    Update project files with navigation component

--- NavigationApp/japp/src/main/java/com/woowrale/navigation/japp/ui/fragment/detail/FragmentDetailViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.woowrale.navigation.japp.ui.fragment.detail;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragmentDetailViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- NavigationApp/japp/src/main/java/com/woowrale/navigation/japp/ui/fragment/home/FragmentHomeViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.woowrale.navigation.japp.ui.fragment.home;
-
-import androidx.lifecycle.ViewModel;
-
-public class FragmentHomeViewModel extends ViewModel {
-
-}
